1b8e0808e6499e40b26a83c5e0c0745742b24445
meisan96 <48047084+meisan96@users.noreply.github.com>
2021-10-06 02:39:41 -0400

    Add files via upload

---
+++ WebSite1/Ejer4.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Ejer4.aspx.cs" Inherits="Ejer4" %>
+
+<%@ Register assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" namespace="System.Web.UI.DataVisualization.Charting" tagprefix="asp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <br />
+        Opcion ver notas de&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server" Width="144px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Ver" Width="62px" />
+        <br />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <br />
+        Opcion de ver Promedios por departamento 
+        <br />
+        <asp:GridView ID="GridView2" runat="server">
+        </asp:GridView>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebSite1/Ejer4.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Data;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+public partial class Ejer4 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        MySqlConnection con = new MySqlConnection();
+        MySqlDataAdapter ada = new MySqlDataAdapter();
+
+        DataSet ds = new DataSet();
+        con.ConnectionString = "Server=localhost;UserID=root;Database=mibasemoises;Password=moises123;";
+        ada.SelectCommand = new MySqlCommand();
+        ada.SelectCommand.Connection = con;
+        ada.SelectCommand.CommandText = "SELECT n.sigla, (SUM(CASE WHEN p.departamento ='01' THEN n.notafinal ELSE 0 END )) /(SUM(CASE WHEN p.departamento ='01' THEN 1 ELSE 0 END )) CH, (SUM(CASE WHEN p.departamento ='02' THEN n.notafinal ELSE 0 END )) /(SUM(CASE WHEN p.departamento ='02' THEN 1 ELSE 0 END )) LP, (SUM(CASE WHEN p.departamento ='03' THEN n.notafinal ELSE 0 END )) /(SUM(CASE WHEN p.departamento ='03' THEN 1 ELSE 0 END )) CB, (SUM(CASE WHEN p.departamento ='04' THEN n.notafinal ELSE 0 END )) /(SUM(CASE WHEN p.departamento ='04' THEN 1 ELSE 0 END )) 'OR', (SUM(CASE WHEN p.departamento ='05' THEN n.notafinal ELSE 0 END )) /(SUM(CASE WHEN p.departamento ='05' THEN 1 ELSE 0 END )) PT, (SUM(CASE WHEN p.departamento ='06' THEN n.notafinal ELSE 0 END )) /(SUM(CASE WHEN p.departamento ='06' THEN 1 ELSE 0 END )) TJ, (SUM(CASE WHEN p.departamento ='07' THEN n.notafinal ELSE 0 END )) /(SUM(CASE WHEN p.departamento ='07' THEN 1 ELSE 0 END )) SC, (SUM(CASE WHEN p.departamento ='08' THEN n.notafinal ELSE 0 END )) /(SUM(CASE WHEN p.departamento ='08' THEN 1 ELSE 0 END )) BE, (SUM(CASE WHEN p.departamento ='09' THEN n.notafinal ELSE 0 END )) /(SUM(CASE WHEN p.departamento ='09' THEN 1 ELSE 0 END )) PD FROM persona p, nota n WHERE p.ci=n.ci GROUP BY n.sigla";
+        ada.Fill(ds);
+        GridView2.DataSource = ds.Tables[0];
+        GridView2.DataBind();
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        MySqlConnection con = new MySqlConnection();
+        MySqlDataAdapter ada = new MySqlDataAdapter();
+
+        DataSet ds1 = new DataSet();
+        con.ConnectionString = "Server=localhost;UserID=root;Database=mibasemoises;Password=moises123;";
+        ada.SelectCommand = new MySqlCommand();
+        ada.SelectCommand.Connection = con;
+        ada.SelectCommand.CommandText = "SELECT * from nota where ci="+TextBox1.Text;
+        ada.Fill(ds1);
+        GridView1.DataSource = ds1.Tables[0];
+        GridView1.DataBind();
+    }
+}
